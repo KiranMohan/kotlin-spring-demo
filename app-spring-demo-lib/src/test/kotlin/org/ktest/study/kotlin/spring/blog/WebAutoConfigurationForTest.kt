f1090a46acdf3ede172f464bc85c5fe5ba0029f7
Kiran Mohan <kiran.mohan@123.com>
2022-01-02 21:02:12 +0530

    fix failing test cases

--- app-spring-demo-lib/src/test/kotlin/org/ktest/study/kotlin/spring/blog/WebAutoConfigurationForTest.kt
+++ app-spring-demo-lib/src/test/kotlin/org/ktest/study/kotlin/spring/blog/WebAutoConfigurationForTest.kt
@@ -1,11 +1,14 @@
 package org.ktest.study.kotlin.spring.blog
 
+import org.ktest.study.kotlin.spring.blog.properties.BlogProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages =  ["org.ktest.study.kotlin.spring.blog.controller"])
+@ComponentScan(basePackages =  ["org.ktest.study.kotlin.spring.blog.controller", "org.ktest.study.kotlin.spring.blog.properties"])
+@EnableConfigurationProperties(BlogProperties::class)
 class WebAutoConfigurationForTest {
 }
